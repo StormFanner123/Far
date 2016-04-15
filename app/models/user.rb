6397c2d6d1366cd72d0814e086019ea31a876901
StormFanner123 <chappyb@gmail.com>
2016-04-15 15:53:34 +1000

    space added

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_one :profile         
+  has_one :profile
+  has_many :team_members
+  has_many :steams, through: :team_members         
 end
 
